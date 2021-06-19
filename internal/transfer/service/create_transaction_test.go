69b656974222a2a94e2e5b2d63634d9e77fa49a2
Joubert RedRat <eu+git@redrat.com.br>
2021-06-19 19:28:36 -0300

    fix create test

--- internal/transfer/service/create_transaction_test.go
+++ internal/transfer/service/create_transaction_test.go
@@ -11,34 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func getCreateTransactionUserRepositoryFake() repository.UserRepository {
-	repo := repository.NewUserRepositoryFake()
-	repo.FakeGetPayerByID = func(ID string) (*entity.Payer, error) {
-		switch ID {
-		case "60f70370-7316-41d5-8980-7ebb9ebb7e68", "0accc07e-ea9a-4b7d-bf99-2060aa2c46bf":
-			return nil, repository.UserRepositoryPayerNotFoundError
-		case "853ea721-09f7-41f9-a6d4-01592f79d40c", "a7733d09-e989-499b-b705-d3dfbd5827f2":
-			return nil, errors.New("fail to connect into fake mongodb")
-		default:
-			payer := entity.NewPayer(ID)
-			return &payer, nil
-		}
-	}
-	repo.FakeGetPayeeByID = func(ID string) (*entity.Payee, error) {
-		switch ID {
-		case "cc5c676d-9363-4c39-b48b-472fd26ff961", "a698ccf1-a416-44ae-bf86-b2ecf23c5763":
-			return nil, repository.UserRepositoryPayeeNotFoundError
-		case "6e4f5873-2833-4ea2-b03e-7ce034fbad1a", "a6d8d6c1-0ec7-46ea-b1a6-e02aac89717d":
-			return nil, errors.New("fail to connect into fake mongodb")
-		default:
-			payee := entity.NewPayee(ID)
-			return &payee, nil
-		}
-	}
-
-	return repo
-}
-
 func TestCreateTransactionHandleCreate(t *testing.T) {
 	tests := []struct {
 		name                     string
@@ -234,4 +206,32 @@ func TestCreateTransactionHandleCreate(t *testing.T) {
 			assert.Equal(t, errorExpected, errorGot)
 		})
 	}
+}
+
+func getCreateTransactionUserRepositoryFake() repository.UserRepository {
+	repo := repository.NewUserRepositoryFake()
+	repo.FakeGetPayerByID = func(ID string) (*entity.Payer, error) {
+		switch ID {
+		case "60f70370-7316-41d5-8980-7ebb9ebb7e68", "0accc07e-ea9a-4b7d-bf99-2060aa2c46bf":
+			return nil, repository.UserRepositoryPayerNotFoundError
+		case "853ea721-09f7-41f9-a6d4-01592f79d40c", "a7733d09-e989-499b-b705-d3dfbd5827f2":
+			return nil, errors.New("fail to connect into fake mongodb")
+		default:
+			payer := entity.NewPayer(ID)
+			return &payer, nil
+		}
+	}
+	repo.FakeGetPayeeByID = func(ID string) (*entity.Payee, error) {
+		switch ID {
+		case "cc5c676d-9363-4c39-b48b-472fd26ff961", "a698ccf1-a416-44ae-bf86-b2ecf23c5763":
+			return nil, repository.UserRepositoryPayeeNotFoundError
+		case "6e4f5873-2833-4ea2-b03e-7ce034fbad1a", "a6d8d6c1-0ec7-46ea-b1a6-e02aac89717d":
+			return nil, errors.New("fail to connect into fake mongodb")
+		default:
+			payee := entity.NewPayee(ID)
+			return &payee, nil
+		}
+	}
+
+	return repo
 }
